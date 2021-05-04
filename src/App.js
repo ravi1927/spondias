ac7b3476225ef577110ce5591bbf6f95c1c6c325
ravichandra <ravi1993272@gmail.com>
2021-05-04 09:51:07 +0530

    add added mainPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './MainPage'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <MainPage />
+        <p>Welcome to Spondias</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/MainPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function MainPage() {
+    return(
+        <>
+        <h1>main page</h1>
+        <MainPage2 />
+        </>
+    )
+}
+
+
+function MainPage2() {
+    return(
+        <h1>main page2</h1>
+    )
+}
+
+export default MainPage;
